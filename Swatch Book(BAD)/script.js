f917032c54f21af880e9a197a13aef86ba3aaa93
Samir <34651762+samirbeydullayev@users.noreply.github.com>
2018-06-08 18:44:36 +0400

    Add files via upload

---
+++ Swatch Book(BAD)/script.js
@@ -0,0 +1,208 @@
+var cards = document.querySelectorAll(".container div");
+var s1 = document.querySelector(".container .s1 ");
+var s2 = document.querySelector(".container .s2 ");
+var s3 = document.querySelector(".container .s3 ");
+var s4 = document.querySelector(".container .s4 ");
+var s5 = document.querySelector(".container .s5 ");
+var s6 = document.querySelector(".container .s6 ");
+var s7 = document.querySelector(".container .s7 ");
+var s8 = document.querySelector(".container .s8 ");
+var s9 = document.querySelector(".container .s9 ");
+var s10 = document.querySelector(".container .s10 ");
+var s11 = document.querySelector(".container .s11 ");
+var s12 = document.querySelector(".container .s12 ");
+
+
+
+
+$(s2).click(function () {
+    s2.style.transform = "rotate(0deg)"
+    s1.style.transform = "rotate(25deg)"
+    s3.style.transform = "rotate(-15deg)"
+    s4.style.transform = "rotate(-30deg)"
+    s5.style.transform = "rotate(-45deg)"
+    s6.style.transform = "rotate(-60deg)"
+    s7.style.transform = "rotate(-75deg)"
+    s8.style.transform = "rotate(-90deg)"
+    s9.style.transform = "rotate(-105deg)"
+    s10.style.transform = "rotate(-120deg)"
+    s11.style.transform = "rotate(-135deg)"
+    s12.style.transform = "rotate(-150deg)"
+
+})
+
+$(s3).click(function () {
+    s3.style.transform = "rotate(0deg)"
+    s1.style.transform = "rotate(40deg)"
+    s2.style.transform = "rotate(25deg)"
+    s4.style.transform = "rotate(-15deg)"
+    s5.style.transform = "rotate(-30deg)"
+    s6.style.transform = "rotate(-45deg)"
+    s7.style.transform = "rotate(-60deg)"
+    s8.style.transform = "rotate(-75deg)"
+    s9.style.transform = "rotate(-90deg)"
+    s10.style.transform = "rotate(-105deg)"
+    s11.style.transform = "rotate(-120deg)"
+    s12.style.transform = "rotate(-135deg)"
+
+})
+
+$(s4).click(function () {
+    s4.style.transform = "rotate(0deg)"
+    s1.style.transform = "rotate(55deg)"
+    s2.style.transform = "rotate(40deg)"
+    s3.style.transform = "rotate(25deg)"
+    s5.style.transform = "rotate(-15deg)"
+    s6.style.transform = "rotate(-30deg)"
+    s7.style.transform = "rotate(-45deg)"
+    s8.style.transform = "rotate(-60deg)"
+    s9.style.transform = "rotate(-75deg)"
+    s10.style.transform = "rotate(-90deg)"
+    s11.style.transform = "rotate(-105deg)"
+    s12.style.transform = "rotate(-120deg)"
+
+})
+
+$(s5).click(function () {
+    s5.style.transform = "rotate(0deg)"
+    s4.style.transform = "rotate(25deg)"
+    s3.style.transform = "rotate(40deg)"
+    s2.style.transform = "rotate(55deg)"
+    s1.style.transform = "rotate(70deg)"
+    s6.style.transform = "rotate(-15deg)"
+    s7.style.transform = "rotate(-30deg)"
+    s8.style.transform = "rotate(-45deg)"
+    s9.style.transform = "rotate(-60deg)"
+    s10.style.transform = "rotate(-75deg)"
+    s11.style.transform = "rotate(-90deg)"
+    s12.style.transform = "rotate(-105deg)"
+
+})
+$(s6).click(function () {
+    s6.style.transform = "rotate(0deg)"    
+    s5.style.transform = "rotate(25deg)"
+    s4.style.transform = "rotate(40deg)"
+    s3.style.transform = "rotate(55deg)"
+    s2.style.transform = "rotate(70deg)"
+    s1.style.transform = "rotate(85deg)"    
+    s7.style.transform = "rotate(-15deg)"
+    s8.style.transform = "rotate(-30deg)"
+    s9.style.transform = "rotate(-45deg)"
+    s10.style.transform = "rotate(-60deg)"
+    s11.style.transform = "rotate(-75deg)"
+    s12.style.transform = "rotate(-90deg)"
+
+
+});
+$(s7).click(function () {
+    s7.style.transform  ="rotate(0deg)"
+    s6.style.transform = "rotate(25deg)"
+    s5.style.transform = "rotate(40deg)"
+    s4.style.transform = "rotate(55deg)"
+    s3.style.transform = "rotate(70deg)"
+    s2.style.transform = "rotate(85deg)" 
+    s1.style.transform = "rotate(100deg)"    
+    s8.style.transform = "rotate(-15deg)"
+    s9.style.transform = "rotate(-30deg)"
+    s10.style.transform = "rotate(-45deg)"
+    s11.style.transform = "rotate(-60deg)"
+    s12.style.transform = "rotate(-75deg)"
+
+});
+
+$(s8).click(function () {
+    s8.style.transform  ="rotate(0deg)"
+    s7.style.transform = "rotate(25deg)"
+    s6.style.transform = "rotate(40deg)"
+    s5.style.transform = "rotate(55deg)"
+    s4.style.transform = "rotate(70deg)"
+    s3.style.transform = "rotate(85deg)" 
+    s2.style.transform = "rotate(100deg)" 
+    s1.style.transform = "rotate(115deg)"           
+    s9.style.transform = "rotate(-15deg)"
+    s10.style.transform = "rotate(-30deg)"
+    s11.style.transform = "rotate(-45deg)"
+    s12.style.transform = "rotate(-60deg)"
+
+});
+
+$(s9).click(function () {
+    s9.style.transform  ="rotate(0deg)"
+    s8.style.transform = "rotate(25deg)"
+    s7.style.transform = "rotate(40deg)"
+    s6.style.transform = "rotate(55deg)"
+    s5.style.transform = "rotate(70deg)"
+    s4.style.transform = "rotate(85deg)" 
+    s3.style.transform = "rotate(100deg)" 
+    s2.style.transform = "rotate(115deg)"
+    s1.style.transform = "rotate(130deg)"                          
+    s10.style.transform = "rotate(-15deg)"
+    s11.style.transform = "rotate(-30deg)"
+    s12.style.transform = "rotate(-45deg)"
+
+});
+
+$(s10).click(function () {
+    s10.style.transform  ="rotate(0deg)"
+    s9.style.transform = "rotate(25deg)"
+    s8.style.transform = "rotate(40deg)"
+    s7.style.transform = "rotate(55deg)"
+    s6.style.transform = "rotate(70deg)"
+    s5.style.transform = "rotate(85deg)" 
+    s4.style.transform = "rotate(100deg)" 
+    s3.style.transform = "rotate(115deg)"
+    s2.style.transform = "rotate(130deg)"
+    s1.style.transform = "rotate(145deg)"                                                        
+    s11.style.transform = "rotate(-15deg)"
+    s12.style.transform = "rotate(-30deg)"
+
+});
+
+$(s11).click(function () {
+    s11.style.transform  ="rotate(0deg)"
+    s10.style.transform = "rotate(25deg)"
+    s9.style.transform = "rotate(40deg)"
+    s8.style.transform = "rotate(55deg)"
+    s7.style.transform = "rotate(70deg)"
+    s6.style.transform = "rotate(85deg)" 
+    s5.style.transform = "rotate(100deg)" 
+    s4.style.transform = "rotate(115deg)"
+    s3.style.transform = "rotate(130deg)"
+    s2.style.transform = "rotate(145deg)" 
+    s1.style.transform = "rotate(160deg)"                                                                                                                   
+    s12.style.transform = "rotate(-15deg)"
+});
+
+$(s12).click(function () {
+    s12.style.transform  ="rotate(0deg)"
+    s11.style.transform = "rotate(25deg)"
+    s10.style.transform = "rotate(40deg)"
+    s9.style.transform = "rotate(55deg)"
+    s8.style.transform = "rotate(70deg)"
+    s7.style.transform = "rotate(85deg)" 
+    s6.style.transform = "rotate(100deg)" 
+    s5.style.transform = "rotate(115deg)"
+    s4.style.transform = "rotate(130deg)"
+    s3.style.transform = "rotate(145deg)" 
+    s2.style.transform = "rotate(160deg)"                                                                                                                   
+    s1.style.transform = "rotate(175deg)"
+});
+
+
+
+$(s1).click(function () {
+    $(cards).toggleClass("rotate")
+
+    s1.style.transform = "rotate(75deg)"
+    s2.style.transform = "rotate(60deg)"
+    s3.style.transform = "rotate(45deg)"
+    s4.style.transform = "rotate(30deg)"
+    s5.style.transform = "rotate(15deg)"
+    s6.style.transform = "rotate(0deg)"
+    s7.style.transform = "rotate(-15deg)"
+    s8.style.transform = "rotate(-30deg)"
+    s9.style.transform = "rotate(-45deg)"
+    s10.style.transform = "rotate(-60deg)"
+    s11.style.transform = "rotate(-75deg)"
+    s12.style.transform = "rotate(-90deg)"
+})
